d52fe55f1d101f7b1dafdfd60dd8a91c6d31a4d6
Harrison Chen <harrisonchen@gmail.com>
2024-03-13 01:45:53 +0800

    some more tensor programming for connect four.  almost done, but haven't tested either.

--- game_learner/connectfour.py
+++ game_learner/connectfour.py
@@ -165,36 +165,28 @@ class C4MDP(MDP):
         return True
     
 
-
 class C4TensorMDP(MDP):
     def __init__(self):
-        # State tensor (num_batches, which_player, player_channel, board_width, board_height)
+        # State tensor (num_batches, player_channel, board_width, board_height)
+        # Mark the game as terminal by turning both players "on"
         # Action tensor (num_batches, num_columns, )
-        super().__init__(None, None, discount=1, num_players=2, state_shape=(2,2,7,6), action_shape=(7,))
+        super().__init__(None, None, discount=1, num_players=2, state_shape=(2,7,6), action_shape=(7,))
         self.symb = {'0': "O", '1': "X", '': "-"}
-        self.swap = torch.tensor([[0,1],[1,0]], dtype=float)
-        self.p0 = torch.tensor([1,0], dtype=float)
-        self.p1 = torch.tensor([0,1], dtype=float)
+        self.penalty = -10000000
 
-    def swap_player(self, state):
-        return torch.tensordot(self.swap, state, ([1], [0]))
-
-    # Assumes the state is valid.
+    # If player 0 has placed more than player 1, then it's player 1's turn.  Otherwise, it's player 0's turn.
     def get_player(self, state):
-        return state.sum((1,2,3)).max(0).indices.item()
-    
+        summed = state.sum((2,3))
+        return 1 * (summed[:,0] > summed[:,1])
 
-    def get_initial_state(self, start_player = 0, batch_size=1):
-        if start_player != 0 and start_player != 1:
-            start_player = random.choice((0,1))
-        batch_factor = torch.ones((batch_size, ), dtype=float)
-        if start_player == 0:
-            return torch.tensordot(batch_factor, torch.tensordot(self.p0, torch.zeros((2,7,6), dtype=float), 0), 0)
-        else:
-            return torch.tensordot(batch_factor, torch.tensordot(self.p1, torch.zeros((2,7,6), dtype=float), 0), 0)
-    
+    # Player 0 is always the first player.
+    def get_initial_state(self, batch_size=1):
+        return torch.tensordot(torch.ones((batch_size, ), dtype=float), torch.zeros((2,7,6), dtype=float), 0)    
 
     def board_str(self, state):
+        # We indicate a terminal state by negating everything
+        if self.is_terminal(state):
+            state = state * -1
         rows = ["|" for i in range(6)]
         p = self.get_player(state)
         out += f"Current player: {self.symb[str(p)]}\n"
@@ -229,54 +221,49 @@ class C4TensorMDP(MDP):
             return self.actions
 
 
+    def is_valid_move(self, state: torch.Tensor, action: torch.Tensor):
+        # Sum the channels and columns, add the action, should be <= 6
+        return state.sum((1,3)) + action <= torch.ones(action.shape, dtype=float) * 6
+
     # Reward is 1 for winning the game, -1 for losing, and 0 for a tie; awarded upon entering terminal state
-    def transition(self, state, a):
-        # Copy it
-        p, s, _ = state
-            
+    # Return tuple of tensors with shape (batch_num, ) + state_shape for the next state and (batch_num, ) for the reward
+    def transition(self, state: torch.Tensor, action: torch.Tensor):
         # Check if move is valid, i.e. if the column is full and if a is in the set of actions
         # If it's not a valid move, give a penalty (a large penality; it should learn to never make these moves)
-        if a not in self.actions or len(s[a]) >= 6:
-            if p == 0:
-                penalty = (-1000, 0,)
-            else:
-                penalty = (0, -1000)
-            return state, penalty
+        if self.is_valid_move(state, action):        
+            return state, self.get_player(state) * self.penalty
         
-        # If the move is valid, make it
-        b = list(s)
-        b[a] += str(p)
-        b = tuple(b)
+        p = self.get_player(state)
 
-        # Check for a new winner, i.e. a winner made by the current move, which can only be the current player
-        # w = (1, -1) if p == 0 else (-1, 1)
-        # w = (1 - 2*p, -1 + 2*p)
-        if self.four(p, b, a):
-            return (1 - p, b, p), (1 - 2*p, -1 + 2*p)
-        else:
-            return (1 - p, b, None), (0, 0)
-    
+        # Make the move!  Note this operation is safe: if the move is invalid, no move will be made.
+        # First, get the position we want to add by summing the channels and columns, resulting in shape (batch, 7); we extend it by ones to shape (batch, 7, 6)
+        # Then, we compare it to tensor of shape (7, 6) to get a tensor of shape (batch, 7, 6), where the 1 is where it's supposed to be
+        newpos = 1. * (torch.tensordot(state.sum((1,3)), torch.ones(6, dtype=float), 0) == torch.tensordot(torch.ones(7, dtype=float), torch.arange(6, dtype=float)))
+        # Then, we put the player channel back in
+        newpiece = torch.swapaxes(torch.tensordot(p, newpos, 0), 0, 1)
+        # Then, we add it to the state
+        newstate = state + newpiece
 
+        # Check for a winner
+        winner = self.has_winner(newstate, newpiece)
+        # Make the state terminal if there is a winner
+        return (winner != 0) * newstate, # TODO
+
+    # TODO: returns a shape (batch, 2) tensor saying who the winner is.  0 if no winner
+    def has_winner(self, newstate, newpiece):
+        pass
     
     # The game is short enough that maybe we don't care about intermediate states
     def get_random_state(self):
         return self.get_initial_state()
 
-    def is_terminal(self, state):
-        return False if state[2] == None else True
+    # Indicate a terminal state by negating everything.  So, if the maximum value of the negation is positive, then it is a terminal state.
+    def is_terminal(self, state: torch.Tensor):
+        return (state * -1).flatten(1,3).max(1).values > 0
     
-    # '0' or '1' for a player, '' for empty
-    def get_by_coords(self, s, row, col):
-        return '' if (row >= len(s[col]) <= row or row < 0) else s[col][row]
-
+    # Sum the player, channels, and board factors.  The result should be 1 * 1 * 6 * 7 = 42.
     def is_full(self, state: torch.Tensor):
-        # Sum the player, channels, and board factors.  The result should be 1 * 1 * 6 * 7 = 42.
         return state.sum((1,2,3,4)) == 42 * torch.ones(state.shape[0], dtype=float)
-
-        for row in s:
-            if len(row) < 6:
-                return False
-        return True
     
 
 
